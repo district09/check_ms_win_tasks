0d9a589dae37310e0d4474f968080b08b822519f
D'Haese Willem <willem.dhaese@gmail.com>
2014-03-23 17:45:18 +0100

    Updated script header

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Check Microsoft Windows Scheduled Tasks
+# Check Microsoft Windows 2008 or Higher Scheduled Tasks
 
 ### Idea
 
@@ -12,6 +12,6 @@ Under development. Confirmed to work on Windows 2008 and Windows 2008 R2.
 
 First, building, testing and reporting bugs are highly appreciated. Please include console's output and reproduction step in your bug report.
 
-## On Nagios Exchange
+### On Nagios Exchange
 
 http://exchange.nagios.org/directory/Plugins/Operating-Systems/Windows/NRPE/Check-Windows-2008-or-Higher-Scheduled-Tasks/details
--- check_ms_win_tasks.ps1
+++ check_ms_win_tasks.ps1
@@ -1,32 +1,55 @@
-﻿#  Scriptname:   	check_ms_win_tasks.ps1
-#  Version:			2.14.3.23b
-#  Created on:    	01/02/2014																			
-#  Author:        	D'Haese Willem
-#  Purpose:       	Check Microsoft Windows scheduled tasks excluding defined folders and defined task patterns
-#                 	Returns state of tasks with name, author, exit code and performance data to Nagios.
-#  To do:			- Add switches to change returned values and output
-#					- Add array parameter with exit codes that should be excluded
-#					- Make parameters non mandatory
-#  History:       	03/02/2014 => Add array as argument with excluded folders
-#					15/02/2014 => Add array as argument with excluded task patterns
-#					03/03/2014 => Added perfdata and edited output
-#					09/03/2014 => Added running tasks information and perfdata
-#					22/03/2014 => Resolved a bug with output treated as perfdata and did some testing with non-mandatory parameters
-#  How to:			1) Put the script in the NSCP scripts folder
-#					2) In the nsclient.ini configuration file, define the script like this:
-#						check_ms_win_tasks=cmd /c echo scripts\check_ms_win_tasks.ps1 $ARG1$ $ARG2$ $ARG3$; exit $LastExitCode | powershell.exe -command -
-#					3) Make a command in Nagios like this:
-#						check_ms_win_tasks => $USER1$/check_nrpe -H $HOSTADDRESS$ -p 5666 -t 60 -c check_ms_win_tasks -a $ARG1$ $ARG2$ $ARG3$
-#					4) Configure your service like this:
-#						- Make use of the above created command
-#						- Parameter 1 should be 'localhost' (did not test with remoting)
-#						- Parameter 2 should be an array of folders to exclude like for example 'Microsoft, Backup'
-#						- Parameter 3 should be an array of task patterns to exclude like for example 'Jeff,"Copy Test"'
-#						- All single quotes need to be included (In Nagios XI)
-#						- Array values with spaces need double quotes (see above example)
-#					5) This script works perfectly in our environment. Please try debugging yourself, use Powergui, use breakpoints and test where things 
-#					   go wrong. Every environment is different. It should work, so please only contact me if you are 100 % sure you found a bug.
-#					   The script is highly adaptable if you want different output etc.
+﻿###################################################################################################
+# Script name:   	check_ms_win_tasks.ps1
+# Version:			2.14.3.23b
+# Created on:    	01/02/2014																			
+# Author:        	D'Haese Willem
+# Purpose:       	Checks Microsoft Windows scheduled tasks excluding defined folders and defined 
+#					task patterns, returning state of tasks with name, author, exit code and 
+#					performance data to Nagios.
+# On Github:		https://github.com/F1regiver/check_ms_win_tasks.ps1
+# To do:			
+#  	- Add switches to change returned values and output
+#  	- Add array parameter with exit codes that should be excluded
+#	- Make parameters non mandatory
+#	- Test remote execution
+# History:       	
+#	03/02/2014 => Add array as argument with excluded folders
+#	15/02/2014 => Add array as argument with excluded task patterns
+#	03/03/2014 => Added perfdata and edited output
+#	09/03/2014 => Added running tasks information and perfdata
+#	22/03/2014 => Resolved a bug with output treated as perfdata
+#	23/03/2014 => Created repository on Github and updated documentation
+# How to:
+#	1) Put the script in the NSCP scripts folder
+#	2) In the nsclient.ini configuration file, define the script like this:
+#		check_ms_win_tasks=cmd /c echo scripts\check_ms_win_tasks.ps1 $ARG1$ $ARG2$ $ARG3$; exit 
+#		$LastExitCode | powershell.exe -command -
+#	3) Make a command in Nagios like this:
+#		check_ms_win_tasks => $USER1$/check_nrpe -H $HOSTADDRESS$ -p 5666 -t 60 -c 
+#		check_ms_win_tasks -a $ARG1$ $ARG2$ $ARG3$
+#	4) Configure your service in Nagios:
+#		- Make use of the above created command
+#		- Parameter 1 should be 'localhost' (did not test with remoting)
+#		- Parameter 2 should be an array of folders to exclude, example 'Microsoft, Backup'
+#		- Parameter 3 should be an array of task patterns to exclude, example 'Jeff,"Copy Test"'
+#		- All single quotes need to be included (In Nagios XI)
+#		- Array values with spaces need double quotes (see above example)
+#		- All three parameters are mandatory for now, use " " if you don't want exclusions
+# Help:
+#	This script works perfectly in our environment on Windows 2008 and Windows 2008 R2 servers. If
+#	you do happen to find an issue, let me know on Github. The script is highly adaptable if you 
+#	want different output etc. I've been asked a few times to make it multilingual, as obviously
+#	this script will only work on English Windows 2008 or higher servers, but as I do not have 
+#	non-English servers at my disposal, I'm not going to implement any other languages..
+# Copyright:
+#	This program is free software: you can redistribute it and/or modify it under the terms of the
+# 	GNU General Public License as published by the Free Software Foundation, either version 3 of 
+#   the License, or (at your option) any later version.
+#   This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+#	without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+# 	See the GNU General Public License for more details.You should have received a copy of the GNU
+#   General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###################################################################################################
 
 
 param(
@@ -62,7 +85,6 @@ function Get-AllTaskSubFolders {
 }
 
 function Check-Array ([string]$str, [string[]]$patterns) {
-#	param($str, [string[]]$patterns)
     foreach($pattern in $patterns) { if($str -match $pattern) { return $true; } }
     return $false;
 }
